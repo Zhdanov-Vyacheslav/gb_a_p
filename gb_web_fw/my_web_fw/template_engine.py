55b0cbbcb898cd923246d3d9b915ca59b4db4827
Zhdanov-Vyacheslav <slava.zhdanov@phkp.ru>
2022-12-19 07:54:06 +0300

    lesson_3

--- gb_web_fw/my_web_fw/template_engine.py
+++ gb_web_fw/my_web_fw/template_engine.py
@@ -1,5 +1,5 @@
 import os
-from jinja2 import Template
+from jinja2 import Template, Environment, FileSystemLoader
 
 from .request import Request
 
@@ -8,12 +8,12 @@ class Engine:
     def __init__(self, base_dir: str, template_dir: str):
         self.template_dir = os.path.join(base_dir, template_dir)
 
-    def get_template_as_string(self, template_name: str) -> str:
-        template_path = os.path.join(self.template_dir, template_name)
-        if not os.path.isfile(template_path):
-            raise Exception("{path} is not file".format(path=template_path))
-        with open(template_path) as f:
-            return f.read()
+    def get_template(self, template_name: str) -> Template:
+        template_path = os.path.join(self.template_dir)
+        file_loader = FileSystemLoader(template_path)
+        environment = Environment(loader=file_loader)
+        template = environment.get_template(template_name)
+        return template
 
 
 def render(request: Request, template_name, **kwargs):
@@ -24,5 +24,5 @@ def render(request: Request, template_name, **kwargs):
         request.settings.get("BASE_DIR"),
         request.settings.get("TEMPLATE_DIR_NAME")
     )
-    template = Template(engine.get_template_as_string(template_name))
+    template = engine.get_template(template_name)
     return template.render(**kwargs)
--- gb_web_fw/view.py
+++ gb_web_fw/view.py
@@ -8,7 +8,10 @@ from my_web_fw.template_engine import render
 
 class HomePage(View):
     def get(self, request: Request, *args, **kwargs) -> Response:
-        body = render(request, "index.html", **kwargs)
+        context = {}
+        date = datetime.now().date()
+        context.setdefault("date", date)
+        body = render(request, "index.html", **context, **kwargs)
         return Response(body=body)
 
     def post(self, request: Request, *args, **kwargs) -> Response:
@@ -27,26 +30,10 @@ class HomePage(View):
             }
         else:
             error.setdefault("error_registration", "Не все поля заполнены")
-        # Данные для вывода в консоль по пункту 4 дз урока 2
-        for_question = {
-            "name": raw_name[0] if raw_name else raw_name,
-            "email": raw_email[0] if raw_email else raw_email,
-            "location": raw_location[0] if raw_location else raw_location,
-            "member": raw_member[0] if raw_member else raw_member
-        }
-        print(for_question)
-        # ///
-        body = render(request, "index.html", **error, **kwargs)
-        return Response(body=body)
+        return self.get(request, *args, **kwargs, **error)
 
 
 class About(View):
     def get(self, request: Request, *args, **kwargs) -> Response:
-        body = "Good info about this site"
-        info = request.GET.get("info")
-        if info:
-            body += "\nВаше info={info}".format(info=info)
-        time = request.GET.get("time")
-        if time:
-            body += "\nТекущее время: {time}".format(time=datetime.now().time().isoformat(timespec="seconds"))
+        body = render(request, "about.html", **kwargs)
         return Response(body=body)
